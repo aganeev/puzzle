dc06ce3db1bd444314ed21ccc76c03d3d23977b1
Artem Ganeev <ag1550@att.com>
2018-05-30 10:14:08 +0300

    Added some test files and outputs

--- src/main/java/jigsaw/puzzle/PuzzleMain.java
+++ src/main/java/jigsaw/puzzle/PuzzleMain.java
@@ -3,15 +3,20 @@ package jigsaw.puzzle;
 import jigsaw.puzzle.entities.Piece;
 import jigsaw.puzzle.entities.Report;
 
+import java.util.Arrays;
 import java.util.Set;
+import java.util.concurrent.TimeUnit;
 
 public class PuzzleMain {
     public static void main(String[] args) {
-        String inputPath = "src/test/resources/solvable2.txt"; //args[0];
-        String outputPath = "target/output.txt"; //args[1];
+        long startTime = System.nanoTime();
+        String inputPath = args[0];
+        String outputPath = args[1];
 
         PuzzleMain puzzleMain = new PuzzleMain();
         puzzleMain.doWork(inputPath, outputPath);
+        long estimatedTime = System.nanoTime() - startTime;
+        System.out.println("Elapsed time: " + TimeUnit.NANOSECONDS.toSeconds(estimatedTime) + " seconds");
 
     }
 
@@ -26,13 +31,16 @@ public class PuzzleMain {
         if (!report.hasErrors() && !pieces.isEmpty()) {
             PuzzleValidator puzzleValidator = new PuzzleValidator(report, pieces);
             Set<int[]> options = puzzleValidator.getOptions();
+            System.out.println("All side options:");
+            options.forEach(option-> System.out.println(Arrays.toString(option)));
             if (!report.hasErrors() && !options.isEmpty()) {
                 Solver solver = new Solver(report, pieces);
-                if (options.stream().noneMatch(solver::hasSolution)) {
+                if (options.stream().peek(option-> System.out.println("Current being handled option: " + Arrays.toString(option))).noneMatch(solver::hasSolution)) {
                     report.addErrorLine("Cannot solve puzzle: it seems that there is no proper solution");
                 }
             }
         }
+        System.out.println("Done");
         outputHandler.reportToFile(outputPath);
     }
 
